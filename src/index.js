0e0d2b4b96068ecaa65689a865156fd2d9b85062
divyans-GitHub <divyanshuji123456@gmail.com>
2022-01-07 20:31:27 +0530

    install redux and create STORE

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+
+const store = createStore(movies);
+console.log("STORE IS: " , store );
+console.log("STATE IN STORE IS: " , store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
